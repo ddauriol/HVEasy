8fec94fedd78438b147478dfdc628344b065758c
Douglas Maciel d'Auriol Souza <ddauriol@gmail.com>
2020-04-07 12:30:37 -0300

    Start Insert Storege Values

--- www/js/medicao.js
+++ www/js/medicao.js
@@ -1,176 +1,75 @@
-
+// esquema dos dados
 var durezaHV = {
-                nome: "",
-                Carga: "",
-                TempoTeste: "",
-                HV: "",
-                valoreMedidos:{
-                    id: "1",
-                    durezaHV: "a"
-                }
-            };
-
+    label: "",
+    date: "",
+    carga: 0,
+    tempoTeste: 0,
+    valoreMedidos: {
+        id: 0,
+        durezaHV: 0
+    }
+};
 
 var Resultados = [];
 var DurezaAcumulado = [];
 var Indexacao = 0;
-var Diagonal="";
-var Carga = "";
-var Unidade = "";
-var HV = "";
-var TempoTeste = "";
 
+// Elementos do Displays
+const frmCarga = document.getElementById("frmCarga")
+const frmDiagonal_1 = document.getElementById("frmDiagonal_1")
+const frmDiagonal_2 = document.getElementById("frmDiagonal_2")
+const frmDiagonal = document.getElementById("frmDiagonal")
+const frmUnidade = document.getElementById("frmUnidade")
+const frmTempo = document.getElementById("frmTempo")
+const frmValorHV = document.getElementById("frmValorHV")
+const frmValorCarga = document.getElementById("frmValorCarga")
+const frmValorTempo = document.getElementById("frmValorTempo")
+const HVtoMPA = document.getElementById("HVtoMPA")
+const HVtoGPA = document.getElementById("HVtoGPA")
+const divResultados = document.getElementById("divResultados")
 
-function CalcularDureza() {
+async function CalcularDureza() {
 
-    if (document.getElementById("frmCarga").value == "") {
+    if (frmCarga.value == "") {
         $('#ModalCarga').modal('show');
-    } else{
+    } else {
 
-        if (document.getElementById("frmDiagonal_1").value == "" || document.getElementById("frmDiagonal_2")
-        .value == "") {
+        if (frmDiagonal_1.value == "" || frmDiagonal_2
+            .value == "") {
             $('#ModalDiagonal').modal('show');
         } else {
-            var D1 = document.getElementById("frmDiagonal_1").value
-            var D2 = document.getElementById("frmDiagonal_2").value
+            var D1 = frmDiagonal_1.value
+            var D2 = frmDiagonal_2.value
+            var Carga = parseFloat(frmCarga.value);
+            var Unidade = parseFloat(frmUnidade.value);
 
-            Diagonal = (parseFloat(D1) + parseFloat(D2)) / 2;
-            document.getElementById("frmDiagonal").value = Diagonal
+            var [HV, Diagonal] = await calcHV(D1,D2, Carga, Unidade)
+            console.log(HV)
+            frmDiagonal.value = Diagonal
+            var TempoTeste = parseFloat(frmTempo.value);
 
-            Carga = parseFloat(document.getElementById("frmCarga").value);
-            Unidade = parseFloat(document.getElementById("frmUnidade").value);
-            HV = 0.1891 * (9.80665 * Carga) / (Diagonal * Diagonal);
-            HV = HV * Unidade;
+            frmValorHV.innerHTML = HV.toFixed(0);
+            frmValorCarga.innerHTML = Carga / 1000;
+            frmValorTempo.innerHTML = TempoTeste;
+            var [GPA, MPA] = await convertMPA(HV)
+            HVtoMPA.innerHTML = MPA
+            HVtoGPA.innerHTML = GPA
 
-            TempoTeste = parseFloat(document.getElementById("frmTempo").value);
-
-            document.getElementById("frmValorHV").innerHTML = HV.toFixed(0);
-            document.getElementById("frmValorCarga").innerHTML = Carga/1000;
-            document.getElementById("frmValorTempo").innerHTML = TempoTeste;
-            document.getElementById("HVtoMPA").innerHTML = (HV.toFixed(0) * 9.807).toFixed(2);
-            document.getElementById("HVtoGPA").innerHTML = (HV.toFixed(0) * 0.009807).toFixed(2);
-
-
-            document.getElementById("divResultados").style.visibility = "visible";
-            // document.getElementById("containerResultados").style.visibility = "visible";
+            divResultados.style.visibility = "visible";
         };
     };
 
 }
 
-function AcumularResultado() {
-
-    if (HV == 0) {
-        return;
-    }
-
-    document.getElementById("containerResultados").style.visibility = "visible";
-    // document.getElementById("divSalvarMedicao").style.visibility = "visible";
-    document.getElementById("frmGrafico").style.visibility = "visible";
-
-    Resultados[Indexacao] = "<tr style='text-align: center'>";
-    Resultados[Indexacao] += "<th scope='row'>" + (Indexacao + 1) + "</th>";
-    Resultados[Indexacao] += "<td>" + HV.toFixed(0) + "</td>";
-    Resultados[Indexacao] += "<td>" + Carga + "</td>";
-    Resultados[Indexacao] += "<td>" + TempoTeste + "</td>";
-    Resultados[Indexacao] += "<td> <button disabled id='btnRemove_" + Indexacao +
-        "' class='btn btn-danger btn-fab btn-round' onclick='RemoverResultado(" + Indexacao +
-        ")'> <i class='material-icons md-18'>delete_sweep</i> </button> </td>";
-    Resultados[Indexacao] += "</tr>";
-    document.getElementById("TabResultados").innerHTML = Resultados;
-
-    DurezaAcumulado[Indexacao] = HV;
-
-    EstatisticaDescritiva();
-
-    if (Indexacao == 0) {
-        document.getElementById("btnRemove_" + Indexacao).disabled = true;
-    } else {
-        document.getElementById("btnRemove_" + Indexacao).disabled = false;
-    }
-
-    // document.getElementById("frmDiagonal_1").focus();
-
-    CarregarDivGraficos();
-
-    Indexacao = Indexacao + 1;
-
-    // Carregando variaveis
-    // CarregarVariaveisDB();
-
+function calcHV(D1, D2, Carga, Unidade) {
+    var Diagonal = (parseFloat(D1) + parseFloat(D2)) / 2;
+    var HV = 0.1891 * (9.80665 * Carga) / (Diagonal * Diagonal);
+    HV = HV * Unidade;
+    return [HV, Diagonal]
 }
 
-function RemoverResultado(i) {
-    Resultados.splice(i, 1);
-    DurezaAcumulado.splice(i, 1);
-    document.getElementById("TabResultados").innerHTML = Resultados;
-
-    EstatisticaDescritiva();
-
-    CarregarDivGraficos();
-
-    Indexacao = Indexacao - 1;
-
-    if (Indexacao == 1) {
-        document.getElementById("btnRemove_" + Indexacao).disabled = true;
-    } else {
-        document.getElementById("btnRemove_" + (Indexacao - 1)).disabled = false;
-    }
-
-    CarregarVariaveisDB();
+function convertMPA(HV){
+    var MPA = (HV.toFixed(0) * 9.807).toFixed(2);
+    var GPA = (HV.toFixed(0) * 0.009807).toFixed(2);
+    return [MPA, GPA]
 }
-
-function EstatisticaDescritiva() {
-
-    document.getElementById("frmStats").innerHTML = "<td>" + math.mean(DurezaAcumulado).toFixed(0) +
-        "</td><td>" + math.max(DurezaAcumulado).toFixed(0) + "</td><td>" + math.min(DurezaAcumulado).toFixed(
-            0) + "</td><td>" + math.std(DurezaAcumulado).toFixed(2) + "</td>";
-
-}
-
-function CarregarDivGraficos() {
-
-    document.getElementById("frmGrafico").innerHTML =
-        "<button id='btnCriarGrafico' type='button' class='btn btn-success btn-lg btn-block' onclick='CriarGrafico()'>Criar Gráfico</button>" +
-        "<canvas id='line-chart' width='100%' height='50'></canvas>";
-
-}
-
-function CarregarVariaveisDB() {
-
-    DBPonto = Indexacao;
-    DBDureza = DurezaAcumulado;
-    DBTempoTeste = parseFloat(document.getElementById("frmValorTempo").value);
-    DBCargaTeste = parseFloat(document.getElementById("frmValorCarga").value);
-
-}
-
-function CriarGrafico() {
-    var labels = [];
-    for (var i = 0; i < DurezaAcumulado.length; i++) {
-        labels[i] = i + 1;
-    };
-    new Chart(document.getElementById("line-chart"), {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                data: DurezaAcumulado,
-                borderColor: "#3e95cd",
-                fill: false
-            }]
-        },
-        options: {
-            title: {
-                display: true,
-                text: 'Dureza'
-            },
-            legend: {
-                display: false,
-                labels: {
-                    fontColor: 'rgb(255, 99, 132)'
-                }
-            }
-        }
-    });
-};
